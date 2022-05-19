6cc08472497c91ab82b1535fbd1bf25a213439a1
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2022-05-19 19:50:04 +0700

    ubah stateful menjadi stateless component

--- src/App.js
+++ src/App.js
@@ -4,60 +4,35 @@ import styles from "./App.module.css";
 import { fetchData } from "./api";
 import coronaImage from "./assets/logo.png";
 
-export class App extends React.Component {
-  // function App() {
-  // const [data, setData] = useState({});
-  // const [country, setCountry] = useState("");
+function App() {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState("");
 
-  // useEffect(() => {
-  //   const fetchAPI = async () => {
-  //     setData(await fetchData());
-  //   };
-  //   fetchAPI();
-  // });
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setData(await fetchData());
+    };
+    fetchAPI();
+  }, []);
 
-  // const handleCountryChange = async (country) => {
-  //   const fetchedData = await fetchDataCountry(country);
-
-  //   setData(fetchedData);
-  //   setCountry(country);
-
-  //   // this.setState({data: fetchedData, country: country})
-
-  //   console.log(country);
-  //   console.log(fetchedData);
-  // };
-
-  state = {
-    data: {},
-    country: "",
-  };
-
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
-  }
-
-  handleCountryChange = async (country) => {
+  const handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
 
-    this.setState({ data: fetchedData, country: country });
+    setData(fetchedData);
+    setCountry(country);
 
-    // console.log(country);
-    // console.log(fetchedData);
+    console.log(country);
+    console.log(fetchData);
   };
 
-  render() {
-    const { data, country } = this.state;
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-    );
-  }
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={coronaImage} alt="COVID-19" />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
+    </div>
+  );
 }
 
 export default App;
